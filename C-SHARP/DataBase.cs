b691f97d756f0d6c39b61de4a8786ae2ec28593d
WiseMaterialismer <1353267372@qq.com>
2021-09-09 17:56:11 +0800

    更新 DataBase类,添加验证码,正则表达式类

--- C-SHARP/DataBase.cs
+++ C-SHARP/DataBase.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using MySql.Data.MySqlClient;
 using th2.bean;
@@ -6,12 +6,12 @@ using th2.config;
 
 namespace th2.other
 {
-    static class DataBase//静态
+    static class DataBase
     {
-        static string _dBConnStr;//数据库连接语句本体
-        static MySqlConnection _mySqlConnection;//连接
+        static string dBConnStr;//数据库连接语句本体
+        static MySqlConnection mySqlConnection;//连接
 
-        //将连接语句更新写入app.config里的connectionStrings标签
+        //将连接语句更新写入app.config里的connectionStrings标签//
         public static bool UpdateConnectionStringsConfig(String newName, String newConString, String newProviderName)
         {
             try
@@ -25,12 +25,12 @@ namespace th2.other
             }
         }
 
-        //从配置文件返回连接语句
+        //从配置文件返回连接语句//
         public static bool GetConnectionStringsConfig()
         {
             try
             {
-                _dBConnStr = DBconfig.GetConnectionStringsConfig("MySqlConn");
+                dBConnStr = DBconfig.GetConnectionStringsConfig("MySqlConn");
                 return true;
             }
             catch (Exception)
@@ -39,20 +39,21 @@ namespace th2.other
             }
         }
 
-        //用语句得到连接
+        //用语句得到连接//
         public static MySqlConnection GetMySqlConnection()
         {
             GetConnectionStringsConfig();
-            _mySqlConnection = new MySqlConnection(_dBConnStr);
-            return _mySqlConnection;
+            mySqlConnection = new MySqlConnection(dBConnStr);
+            return mySqlConnection;
         }
 
+        //开启数据库//
         public static bool DatabaseOpen()
         {
             GetMySqlConnection();
             try
             {
-                _mySqlConnection.Open();
+                mySqlConnection.Open();
                 return true;
             }
             catch (Exception)
@@ -61,16 +62,17 @@ namespace th2.other
             }
             finally
             {
-                _mySqlConnection.Close();
+                mySqlConnection.Close();
             }
         }
 
+        //得到没有被Read过的Read数据//
         public static MySqlDataReader GetSqlDataRead0(string sql)
         {
             try
             {
-                _mySqlConnection.Open();
-                var sqlCommand = new MySqlCommand(sql, _mySqlConnection);
+                mySqlConnection.Open();
+                var sqlCommand = new MySqlCommand(sql, mySqlConnection);
                 var sqlRead0 = sqlCommand.ExecuteReader();
                 return sqlRead0;
             }
@@ -79,6 +81,7 @@ namespace th2.other
                 return null;
             }
         }
+        //Read过一次的Read数据//
         public static MySqlDataReader GetSqlDataRead1(string sql)
         {
             MySqlDataReader sqlRead0 = GetSqlDataRead0(sql);
@@ -87,8 +90,8 @@ namespace th2.other
             return sqlRead1;
         }
 
-
-        public static bool IsUserLogin(string user, string userPhone, string password)//登录
+        //登录//
+        public static bool IsUserLogin(string user, string userPhone, string password)
         {
             String sqlLogin = string.Format("select * from user where (user = '{0}'and password = '{1}')or (user_phone = '{2}'and password = '{1}')", user, password, userPhone);//SQL语句查找账号密码
             try
@@ -109,12 +112,12 @@ namespace th2.other
             }
             finally
             {
-                _mySqlConnection.Close();
+                mySqlConnection.Close();
             }
         }
 
-
-        public static bool UserLogOn(string user, string userName, string password, string userPhone, string power)//注册新账户
+        //注册新账户//
+        public static bool UserLogOn(string user, string userName, string password, string userPhone, string power)
         {
             string sqlLogon = string.Format(@"INSERT INTO user(user,user_name,password,user_phone,power) VALUES('{0}','{1}','{2}','{3}','{4}')", user, userName, password, userPhone, power);
             try
@@ -128,14 +131,12 @@ namespace th2.other
             }
             finally
             {
-                _mySqlConnection.Close();
+                mySqlConnection.Close();
             }
         }
 
-
-        public static void SelectAllUserInfo()//查询全部用户信息//
-        //由于在GetSqlRead里Read过一次,当在while循环中第一次Read时,实为第二次Read,固没有查询第列数据,
-        //可把第一列数据当作测试数据
+        //查询全部用户信息//
+        public static void SelectAllUserInfo()
         {
             string sqlSelectAllUserInfo = "select * from user";//查询全部用户信息的sql语句"从user表查询全部数据"
             MySqlDataReader allUserInfoRead = GetSqlDataRead0(sqlSelectAllUserInfo);//将sql语句调入到GetSqlDataRead中得到DataReader数据
@@ -147,9 +148,10 @@ namespace th2.other
                 UserInfo.addAllInfoList();
                 //一个用户全部的信息被添加完毕,将这个用户信息当成一个信息添加到userInfoList集合中
             }
-            _mySqlConnection.Close();
+            mySqlConnection.Close();
         }
 
+        //查询所有的User//
         public static List<string> SelectAllUser()
         {
             List<string> allUserList = new List<string>();
@@ -160,12 +162,12 @@ namespace th2.other
                 string userListInfo = allUserReader.GetString(0);
                 allUserList.Add(userListInfo);
             }
-            _mySqlConnection.Close();
+            mySqlConnection.Close();
             return allUserList;
         }
 
-
-        public static void SelectAllLogInfo() //查询全部日志信息
+        //查询全部日志信息//
+        public static void SelectAllLogInfo() 
         {
             LogInfo logInfo = new LogInfo();
             string sqlSelectAllLogInfo = "select * from log_info";
@@ -175,10 +177,11 @@ namespace th2.other
                 logInfo.setLoginInfoList(allLogInfoRead.GetInt32(0),allLogInfoRead.GetString(2),allLogInfoRead.GetString(3),allLogInfoRead.GetString(4));
                 logInfo.addAllLoginInfo();
             }
-            _mySqlConnection.Close();
+            mySqlConnection.Close();
         }
 
-        public static bool UpdatePassword(Int32 id, string password)//修改密码
+        //修改密码//
+        public static bool UpdatePassword(Int32 id, string password)
         {
             string sqlAlterPassword = string.Format("update user set password = '{0}' where id = {1}", password, id);
             try
@@ -192,10 +195,76 @@ namespace th2.other
             }
             finally
             {
-                _mySqlConnection.Close();
+                mySqlConnection.Close();
             }
         }
-        public static void RecordLog() //日志
+
+        //通过User,UserName,userPhone验证账号//
+        public static int VerifyUser(string fgUserText, string fgNameText, string fgPhoneText)
+        {
+            string sqlVerifyUser =
+                string.Format(@"select id from user where ( user = '{0}' and user_name = '{1}' ) and user_phone = '{2}'",
+                    fgUserText, fgNameText, fgPhoneText);
+            try
+            {
+                MySqlDataReader verifyUserRead = GetSqlDataRead1(sqlVerifyUser);
+                return verifyUserRead.GetInt32(0);
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+            finally
+            {
+                mySqlConnection.Close();
+            }
+        }
+
+        //根据user删除账号//
+        public static bool DeleteUserByUser(string user)
+        {
+            string sqlDeleteById = string.Format(@"delete from user where user = '{0}'", user);
+
+            try
+            {
+                MySqlDataReader deleteByIdRead = GetSqlDataRead0(sqlDeleteById);
+                deleteByIdRead.Read();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            finally
+            {
+                mySqlConnection.Close();
+            }
+        }
+
+        //编辑账号//
+        public static bool UpdateUserByUser(string userName, string password, string userPhone, string power, string user) 
+        {
+            string sqlUpdateByUser = string.Format(@"update user set user_name = '{0}', password = '{1}',user_phone = '{2}',power = '{3}' where user = '{4}'",userName,password,userPhone,power,user);
+            try
+            {
+                MySqlDataReader updateUserByUserReader = GetSqlDataRead0(sqlUpdateByUser);
+                updateUserByUserReader.Read();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            finally
+            {
+                mySqlConnection.Close();
+            }
+
+        }
+
+
+        //记录日志//
+        public static void RecordLog()
         {
             string sqlRecordLog =
                 string.Format(@"INSERT INTO log_info (login_id, login_user, login_name) VALUES ({0}, '{1}', '{2}');",
@@ -215,69 +284,11 @@ namespace th2.other
             }
             finally
             {
-                _mySqlConnection.Close();
+                mySqlConnection.Close();
             }
         }
 
-        public static int VerifyUser(string fgUserText, string fgNameText, string fgPhoneText)//验证账号
-        {
-            string sqlVerifyUser =
-                string.Format(@"select id from user where ( user = '{0}' and user_name = '{1}' ) and user_phone = '{2}'",
-                    fgUserText, fgNameText, fgPhoneText);
-            try
-            {
-                MySqlDataReader verifyUserRead = GetSqlDataRead1(sqlVerifyUser);
-                return verifyUserRead.GetInt32(0);
-            }
-            catch (Exception)
-            {
-                return 0;
-            }
-            finally
-            {
-                _mySqlConnection.Close();
-            }
-        }
-
-        public static bool DeleteUserByUser(string user)//删除账号
-        {
-            string sqlDeleteById = string.Format(@"delete from user where user = '{0}'", user);
-
-            try
-            {
-                MySqlDataReader deleteByIdRead = GetSqlDataRead0(sqlDeleteById);
-                deleteByIdRead.Read();
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-            finally
-            {
-                _mySqlConnection.Close();
-            }
-        }
-
-        public static bool UpdateUserByUser(string userName, string password, string userPhone, string power, string user) //编辑账号
-        {
-            string sqlUpdateByUser = string.Format(@"update user set user_name = '{0}', password = '{1}',user_phone = '{2}',power = '{3}' where user = '{4}'",userName,password,userPhone,power,user);
-            try
-            {
-                MySqlDataReader updateUserByUserReader = GetSqlDataRead0(sqlUpdateByUser);
-                updateUserByUserReader.Read();
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-            finally
-            {
-                _mySqlConnection.Close();
-            }
-
-        }
+        //根据id删除日志//
         public static bool DeleteLogById(string id)
         {
             string sqlDeleteById = string.Format(@"delete from log_info where id = '{0}'", id);
@@ -294,8 +305,75 @@ namespace th2.other
             }
             finally
             {
-                _mySqlConnection.Close();
+                mySqlConnection.Close();
             }
         }
+
+        //记录登录信息//
+        public static void RecordAstrict()
+        {
+            string sqlRecordLog =
+                string.Format(@"INSERT INTO login_astrict (user) VALUES ({0});",
+                    UserInfo.User);
+            try
+            {
+
+                GetSqlDataRead0(sqlRecordLog);
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+            finally
+            {
+                mySqlConnection.Close();
+            }
+        }
+
+        //检查现存的登录信息个数//
+        public static int selectAstrictNum()
+        {
+            string sqlSelectAstrictNum = @"select * from login_astrict";
+            int i = 0;
+            try
+            {
+                MySqlDataReader mySqlDataReader = GetSqlDataRead0(sqlSelectAstrictNum);
+                while (mySqlDataReader.Read())
+                {
+                    i++;
+                }
+                return i;
+            }
+            catch (Exception )
+            {
+                return 0;
+            }
+            finally
+            {
+                mySqlConnection.Close();
+            }
+        }
+
+        //根据id删除登录信息//
+        public static bool DeleteAstrictById()
+        {
+            string sqlDeleteById = string.Format(@"delete from login_astrict where id = '{0}'", UserInfo.Id);
+
+            try
+            {
+                MySqlDataReader deleteByIdRead = GetSqlDataRead0(sqlDeleteById);
+                deleteByIdRead.Read();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            finally
+            {
+                mySqlConnection.Close();
+            }
+        }
+
     }
 }
